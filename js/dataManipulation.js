ed4d5c1a72826c7da57206803bb1b60c688722c7
ProishTheIdiot <64977705+thepoopooman652@users.noreply.github.com>
2025-12-18 09:51:20 -0500

    Update dataManipulation.js

--- js/dataManipulation.js
+++ js/dataManipulation.js
@@ -26,3 +26,11 @@ const isEmpty = (obj) => {
 const generateRandom = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+function ranStr(length) {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
